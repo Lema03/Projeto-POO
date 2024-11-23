975d3f2d0aa3fbb82f9b1a1814d3aa9167432432
Lema03 <luiseduardoma@hotmail.com>
2024-11-23 19:51:05 -0300

    Add files via upload

--- core/src/main/java/com/jogonba/cartas/board/Tabuleiro.java
+++ core/src/main/java/com/jogonba/cartas/board/Tabuleiro.java
@@ -1,46 +1,54 @@
 package com.jogonba.cartas.board;
-import com.badlogic.gdx.math.Vector2;
 import com.jogonba.cartas.cards.Carta;
-import com.jogonba.cartas.players.Deck;
-import com.jogonba.cartas.players.Jogador;
-import com.jogonba.cartas.players.Hand;
 import java.util.ArrayList;
 
 public class Tabuleiro {
     private ArrayList<Carta> slotsTabuleiro;
-    private ArrayList<Vector2> slotsPosicao;
 
     public Tabuleiro() {
         this.slotsTabuleiro = new ArrayList<>();
-        this.slotsPosicao = new ArrayList<>();
     }
 
     //Métodos relevantes:
-    public void colocarCarta(Carta carta){
-        slotsTabuleiro.add(carta);
-    }
-
     public Carta removerCarta(int posicao) {
         return slotsTabuleiro.remove(posicao);
     }
 
-    public int verificarPosicao(Carta carta){
+    public int verificarPosicao(Carta carta) {
         return slotsTabuleiro.indexOf(carta);
     }
 
-    public ArrayList<Carta> getslotsTabuleiro(){
-        return slotsTabuleiro;
+    //metodo incompleto, pois requer interação com jogador
+    public Carta escolherCarta(int n) {
+        Carta carta = slotsTabuleiro.get(n);
+        return carta;
     }
 
-    public void criarPosicoesCB() {
-        for (int i = 0; i < 5; i++) {
-            slotsPosicao.add(new Vector2(300, 100 * i + 120));
+    public boolean lugarOcupado(int posicao) {
+        if (slotsTabuleiro.get(posicao) == null) {
+            return true;
+        } else {
+            return false;
         }
     }
 
-    public void criarPosicoesMH() {
-        for (int i = 0; i < 5; i++) {
-            slotsPosicao.add(new Vector2(100, 100 * i + 120));
+    public void colocarCarta(int posicao, Carta carta){
+        if (lugarOcupado(posicao)){
+            slotsTabuleiro.add (posicao, carta);
+        } else {
+            System.out.println("O lugar selecionado já está ocupado");
         }
+    }
+
+    public void mostrarTabuleiro() {
+        for (int n = 0; n < slotsTabuleiro.size(); n++) {
+            Carta carta = slotsTabuleiro.get(n);
+            System.out.println(carta);
+        }
+    }
+
+    public int getTabuleiroSize(){
+        slotsTabuleiro.trimToSize();
+        return slotsTabuleiro.size();
     }
 }
